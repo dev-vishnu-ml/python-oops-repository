65f5762e992408476df042f471aaa9aa1b1507fa
dev-vishnu-ml <vishnusharma0531@gmail.com>
2026-08-25 11:43:10 +0530

    single inheritance

--- assignment problems 3/single inheritance.py
+++ assignment problems 3/single inheritance.py
@@ -1,1 +1,15 @@
 # single inheritance
+class Employee:
+    start_time = "9am"
+    End_time = "4pm"
+    def change_start_time(self,new_time):
+        self.start_time = new_time
+
+class Teacher(Employee):
+    def __init__(self,subject):
+        self.subject = subject
+
+t1 = Teacher("python")
+t1.change_start_time("10am")
+print(t1.start_time, t1.End_time, t1.subject)
+
